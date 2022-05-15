3682aaa509262bb3a253f82e1c0facb61dc6f3b8
Taha Babar <tahababar70@gmail.com>
2022-05-15 13:37:28 -0400

    Add files via upload

---
+++ WebsiteToApp/WebsiteToApp/ViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.google.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }}
---
+++ WebsiteToApp/WebsiteToAppUITests/WebsiteToAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WebsiteToAppUITestsLaunchTests.swift
+//  WebsiteToAppUITests
+//
+//  Created by Taha Babar on 4/25/22.
+//
+
+import XCTest
+
+class WebsiteToAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
